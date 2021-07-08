fa73c92019f3ea47bb5fa5019d7e7c9113ae882a
AmirHosseinFallahmanesh <54832303+AmirHosseinFallahmanesh@users.noreply.github.com>
2021-07-08 23:51:28 +0430

    Update ExecptionInterceptor.cs
    test

--- Exeption-Interceptor/ExecptionInterceptor.cs
+++ Exeption-Interceptor/ExecptionInterceptor.cs
@@ -6,11 +6,52 @@ using System.Threading.Tasks;
 
 namespace Exeption_Interceptor
 {
+    
+    public interface IExeptionTrasnlator{
+         Exception Translate(Exception exception);
+        
+        
+    }
+    public class ExceptionTranslator{
+        
+        
+     
+         public Exception Translate(Exception exception)
+         {
+          vart message=   exception.ExtractFullmessage();
+           var ex=Activator.CreateInstance(exception)
+             return  ex;
+         }
+        
+    }
+    
+    
     public class ExecptionInterceptor : IInterceptor
     {
+       IExeptionTrasnlator transltor;
+    
+        public ExecptionInterceptor(ILogger logger,IExeptionTrasnlator transltor){
+        
+        transltor=transltor;
+        
+        }
+    
         public void Intercept(IInvocation invocation)
         {
+            try
+            {
+            
             invocation.Proceed();
+            
+            }
+            catch((Exception  ex )
+            {
+            
+          var translate=  transltor.trasnlate(ex);
+          throw translate;
+            
+            }
+            
         }
     }
 }
