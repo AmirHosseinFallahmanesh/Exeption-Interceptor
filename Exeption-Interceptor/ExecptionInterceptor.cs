04e71a1de2192de4e6f88ee3077199ea83fa22bb
negar mesri <negarmesry@gmail.com>
2021-07-08 17:45:14 +0430

    Add project files.

---
+++ Exeption-Interceptor/ExecptionInterceptor.cs
@@ -0,0 +1,16 @@
+﻿using Castle.DynamicProxy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Exeption_Interceptor
+{
+    public class ExecptionInterceptor : IInterceptor
+    {
+        public void Intercept(IInvocation invocation)
+        {
+            invocation.Proceed();
+        }
+    }
+}
---
+++ Exeption-Interceptor/Models/StdSrevice.cs
@@ -0,0 +1,21 @@
+﻿using Autofac.Extras.DynamicProxy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Exeption_Interceptor.Models
+{
+    [Intercept(typeof(ExecptionInterceptor))]
+    public class StdSrevice: IStdSrevice
+    {
+        public void Print()
+        {
+
+        }
+    }
+    public interface IStdSrevice
+    {
+         void Print();
+    }
+}
